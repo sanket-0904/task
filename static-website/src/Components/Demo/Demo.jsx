f709cd9cfc16b8072781c1b34c78f7968aa69756
sanket-0904 <sablesanket34@gmail.com>
2025-07-23 17:30:24 +0530

    Add Demo component and styles for digital marketing course section

---
+++ static-website/src/Components/Demo/Demo.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Demo.css";
+import bg from "./../../assets/bg.jpg";
+
+const Demo = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="demo">
+          <h1>get a demo</h1>
+          <img src={bg} alt="" />
+        </div>
+        <div className="box">
+          <h2>
+            Up-skill, Earn Certifications, and Get Ready for a Digital Future!
+          </h2>
+          <div className="check-box">
+            <div className="check">
+              <input type="checkbox" />
+              <label>Key Points</label>
+            </div>
+            <div className="check">
+              <input type="checkbox" />
+              <label>Key Points</label>
+            </div>
+            <div className="check">
+              <input type="checkbox" />
+              <label>Key Points</label>
+            </div>
+            <div className="check">
+              <input type="checkbox" />
+              <label>Key Points</label>
+            </div>
+          </div>
+          <div className="input">
+            <input type="text" placeholder="Enter Your Full Name" />
+            <input type="text" placeholder="Enter Your E-mail Address" />
+            <input type="text" placeholder="Contact Number" />
+            <input type="text" placeholder="Select" />
+          </div>
+          <div className="box-btn">
+            <button className="btns">Enroll Now</button>
+            <i class="fa-solid fa-arrow-left fa-bounce"></i>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Demo;
--- static-website/src/Pages/Hero.jsx
+++ static-website/src/Pages/Hero.jsx
@@ -3,12 +3,14 @@ import "./Hero.css";
 import Navbar from "../Components/Navbar/Navbar";
 import Content from "../Components/Content/Content";
 import About from "../Components/About/About";
+import Demo from "../Components/Demo/Demo";
 
 const Hero = () => {
   return (
     <>
       <Content />
       <About />
+      <Demo />
     </>
   );
 };
